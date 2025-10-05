d0623bbc703967a4aad15e3aff13b2eb9f99f970
Lars <l.m.johnen@gmail.com>
2025-10-05 10:42:50 +0200

    removed debug prints

--- Utils/DatabaseUtils.lua
+++ Utils/DatabaseUtils.lua
@@ -11,13 +11,11 @@ function databaseUtils:CheckAndUpdate(database, defaults)
     for key, value in pairs(defaults) do
         if type(value) == "table" then
             if database[key] == nil then
-                print("Setting default for missing key (table):", key)
                 database[key] = {}
             end
             self:CheckAndUpdate(database[key], value)
         else
             if database[key] == nil then
-                print("Setting default for missing key:", key, "=", tostring(value))
                 database[key] = value
             end
         end
