b5edd63e277868c6c4f2da86271e5811babfbc1d
Lars <l.m.johnen@gmail.com>
2025-10-07 23:55:54 +0200

    fixed auto-quest

--- Utils/QuestUtils.lua
+++ Utils/QuestUtils.lua
@@ -27,6 +27,10 @@ function questUtils:Init()
         self:OnQuestDetail()
     end)
 
+    addon:RegisterEvent("QUEST_PROGRESS", "QuestUtils_QuestProgress", function()
+        self:OnQuestProgress()
+    end)
+
     self:CreateSettings()
 end
 
@@ -76,8 +80,6 @@ end
 
 function questUtils:OnQuestComplete()
     if self:IsActive("autoTurnIn") then
-        if GetNumQuestRewards() > 0 then return end
-
         ---@diagnostic disable-next-line: param-type-mismatch
         GetQuestReward(nil)
     end
@@ -86,5 +88,11 @@ end
 function questUtils:OnQuestDetail()
     if self:IsActive("autoAccept") then
         AcceptQuest()
+    end
+end
+
+function questUtils:OnQuestProgress()
+    if self:IsActive("autoTurnIn") then
+        CompleteQuest()
     end
 end
